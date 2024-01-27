357454eba4ae9e61886ec0596abc6c91764bce2f
Jan Niezgoda <jniezgoda21@gmail.com>
2024-01-27 22:17:27 +0100

    Fix syntax error

--- api/schemas.py
+++ api/schemas.py
@@ -3,6 +3,7 @@ from marshmallow import INCLUDE, Schema, fields, post_dump, ValidationError, val
 from api.models import Category, Part
 from marshmallow.validate import Range
 
+
 class KeepUnknownsSchema(Schema):
     class Meta:
         # This keeps unknown fields through schema.load(), but not schema.dump()
@@ -34,8 +35,14 @@ class PartSchema(KeepUnknownsSchema):
     name = fields.String(required=True)
     description = fields.String(required=True)
     category = fields.String(required=True)
-    quantity = fields.Integer(required=True, validate=Range(min_inclusive=1, error="Quantity must be greater than 0")))
-    price = fields.Float(required=True, validate=Range(min_inclusive=0.01, error="Price must be greater or equal 0.01")))
+    quantity = fields.Integer(
+        required=True,
+        validate=Range(min_inclusive=1, error="Quantity must be greater than 0"),
+    )
+    price = fields.Float(
+        required=True,
+        validate=Range(min_inclusive=0.01, error="Price must be greater or equal 0.01"),
+    )
     location = fields.Nested(NestedLocationSchema, required=True)
 
     @validates("serial_number")
